621c78dbc3a731a90b74e5e5e72e65f8cc5b751b
Mathieu Ponce <poncemathieu@gmail.com>
2021-05-05 22:43:15 +0200

    commencement projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlidebarComponent } from './slidebar/slidebar.component';
+import { CardMenuComponent } from './card-menu/card-menu.component';
+import { CardProductComponent } from './card-product/card-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlidebarComponent,
+    CardMenuComponent,
+    CardProductComponent
   ],
   imports: [
     BrowserModule,
